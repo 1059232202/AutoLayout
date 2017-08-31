1e17a2e15c23167bbcd5ac8af1a9996d490cbff4
歌神 <lee1059232202@163.com>
2017-08-31 11:46:05 +0800

    Initial Commit

---
+++ AutoLayout自动布局/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayout自动布局
+//
+//  Created by lilin on 2017/8/31.
+//  Copyright © 2017年 lilin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoLayout自动布局Tests/AutoLayout____Tests.m
@@ -0,0 +1,39 @@
+//
+//  AutoLayout____Tests.m
+//  AutoLayout自动布局Tests
+//
+//  Created by lilin on 2017/8/31.
+//  Copyright © 2017年 lilin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayout____Tests : XCTestCase
+
+@end
+
+@implementation AutoLayout____Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
